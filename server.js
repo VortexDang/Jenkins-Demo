12e614bdd812d5ea707c2deafaa5987682bc8bee
Đăng Trần <tranhaidang.145@gmail.com>
2023-08-16 11:17:23 +0700

    Config Jenkinsfile

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
-console.log("env",process.env.MYSQL_DATABASE)
+console.log("MYSQL_HOST",process.env.MYSQL_HOST)
+console.log("MYSQL_USER",process.env.MYSQL_USER)
+console.log("MYSQL_PASSWORD",process.env.MYSQL_PASSWORD)
+console.log("MYSQL_DATABASE",process.env.MYSQL_DATABASE)
 
 function handleDisconnect() {
   const db = mysql.createConnection({
